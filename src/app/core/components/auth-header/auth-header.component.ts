03cdd9199d3769422e9ded592977ad10bcc6159a
Denis Kalinin <deniskalinin505@gmail.com>
2023-10-20 10:48:12 +0300

    Updated header,auth-header components, hidded some links based on сurrent user role.

--- src/app/core/components/auth-header/auth-header.component.ts
+++ src/app/core/components/auth-header/auth-header.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {AuthService} from "../../services/auth.service";
+import {CourseService} from "../../services/course.service";
+import {Store} from "@ngrx/store";
+import * as fromSelectors from "../../selectors/role-selector";
+import {Router} from "@angular/router";
+import {TranslateService} from "@ngx-translate/core";
+import {ProfileService} from "../../services/profile-service.service";
 
 @Component({
   selector: 'app-auth-header',
@@ -8,8 +14,24 @@ import {AuthService} from "../../services/auth.service";
   styleUrls: ['./auth-header.component.css']
 })
 
-export class AuthHeaderComponent extends HeaderComponent implements OnInit {
+export class AuthHeaderComponent extends HeaderComponent implements OnInit
+{
+  public currentUserRole!: string;
+  constructor(protected override authService : AuthService,
+              protected override router : Router,
+              protected override translate : TranslateService,
+              private profileService : ProfileService,
+              private store : Store) {
+    super(authService, router, translate);
 
-
+    this.store.select(fromSelectors.selectRole).subscribe(async role => {
+      if (role == undefined) {
+        this.currentUserRole = await this.profileService.getCachedInfo();
+        console.log(this.currentUserRole);
+      } else {
+        this.currentUserRole = role;
+      }
+    });
+  }
 
 }
